d5547f87126d3464646db26b40d91bd24682f8fe
bsdayo <bsdayo@outlook.com>
2025-02-24 10:45:17 +0800

    feat: add local search functionality

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -60,6 +60,7 @@ export default defineConfigWithTheme<ThemeConfig>({
 
     nav: [
       { text: '文章', link: '/posts/' },
+      // { text: '文档', link: '/docs/' },
       { text: '工具', link: '/tools/' },
       { text: '友链', link: '/links/' },
       { text: '关于', link: '/about/' },
@@ -71,6 +72,39 @@ export default defineConfigWithTheme<ThemeConfig>({
       { icon: 'x', link: 'https://twitter.com/konobsdayo' },
       { icon: 'github', link: 'https://github.com/bsdayo' },
     ],
+
+    search: {
+      provider: 'local',
+      options: {
+        detailedView: true,
+        _render(src, env, md) {
+          const html = md.render(src, env)
+          if (env.frontmatter?.title) return md.render(`# ${env.frontmatter.title}`) + html
+          return html
+        },
+        translations: {
+          button: {
+            buttonText: '搜索',
+            buttonAriaLabel: '搜索',
+          },
+          modal: {
+            displayDetails: '显示详细列表',
+            resetButtonTitle: '重置搜索',
+            backButtonTitle: '关闭搜索',
+            noResultsText: '没有结果',
+            footer: {
+              selectText: '选择',
+              selectKeyAriaLabel: '输入',
+              navigateText: '导航',
+              navigateUpKeyAriaLabel: '上箭头',
+              navigateDownKeyAriaLabel: '下箭头',
+              closeText: '关闭',
+              closeKeyAriaLabel: 'esc',
+            },
+          },
+        },
+      },
+    },
 
     giscus: {
       host: 'https://giscus.app',
